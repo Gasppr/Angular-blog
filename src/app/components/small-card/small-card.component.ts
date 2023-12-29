f50e09c7757d9815d1bc9aa2417dbe64219ad2d3
Paulo Roberto Antunes De Carvalho <paulorantunes13@hotmail.com>
2023-12-29 02:28:15 -0300

    Style - Implementação da comunicação entre componentes e desacoplamento
    Description:  Criando uma dinâmica entre os componentes para a comunicação de dados transferidos de um componente parent para child

--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
@@ -7,4 +7,14 @@ import { Component } from '@angular/core';
 })
 export class SmallCardComponent {
 
+  @Input()
+  cardTitle : string = ''
+
+  @Input()
+  cardPhoto : string = ''
+
+
+   constructor() {
+
+  }
 }
